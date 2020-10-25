83f2e25172cd935183589de43afdc8bf4b25b22d
johnny <jingpeng@cn.ibm.com>
2020-10-25 20:20:07 +0800

    stage

--- src/pages/test/test.vue
+++ src/pages/test/test.vue
@@ -162,6 +162,17 @@
         <view style="padding: 30rpx"> 折叠内容主体，可自定义内容及样式 </view>
       </uni-collapse-item>
     </uni-collapse>
+
+    <view>
+      <button
+        class="sys_btn"
+        open-type="getUserInfo"
+        lang="zh_CN"
+        @getuserinfo="appLoginWx"
+      >
+        小程序登录授权
+      </button>
+    </view>
   </view>
 </template>
 
@@ -230,6 +241,41 @@ export default {
     }
   },
   methods: {
+    appLoginWx() {
+      // #ifdef MP-WEIXIN
+      uni.getProvider({
+        service: "oauth",
+        success: function (res) {
+          if (~res.provider.indexOf("weixin")) {
+            uni.login({
+              provider: "weixin",
+              success: (res2) => {
+                uni.getUserInfo({
+                  provider: "weixin",
+                  success: (info) => {
+                    //这里请求接口
+                    console.log(res2);
+                    console.log(info);
+                  },
+                  fail: () => {
+                    uni.showToast({ title: "微信登录授权失败", icon: "none" });
+                  },
+                });
+              },
+              fail: () => {
+                uni.showToast({ title: "微信登录授权失败", icon: "none" });
+              },
+            });
+          } else {
+            uni.showToast({
+              title: "请先安装微信或升级版本",
+              icon: "none",
+            });
+          }
+        },
+      });
+      //#endif
+    },
     ...mapMutations(["login"]),
     guideToLogin() {
       uni.showModal({
